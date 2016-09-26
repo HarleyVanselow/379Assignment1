4cb695b765f3eaafa2ddf5269d29b9b894142188
Kdan <kdanhillier@hotmail.co.uk>
2016-09-26 15:18:59 -0600

    seg faults everywhere!

--- memlayout.c
+++ memlayout.c
@@ -1,11 +1,16 @@
 #include <setjmp.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 #define MEM_RW 0
 #define MEM_RO 1
 #define MEM_NO 2
 #define PAGE_SIZE 4096
 
-uint32_t current_memory_pointer=0;
+unsigned int current_memory_pointer = 0;
 sigjmp_buf env;
 
 struct memregion {
@@ -15,7 +20,7 @@ struct memregion {
 };
 void handle_seg_fault(int sig)
 {
-	printf("Encountered seg fault at %d",current_memory_pointer);
+	printf("Encountered seg fault at %d\n",current_memory_pointer);
 	current_memory_pointer+=PAGE_SIZE;
 	siglongjmp(env,1);
 }
@@ -24,12 +29,20 @@ int get_mem_layout (struct memregion *regions, unsigned int size)
 {
 	(void) signal(SIGSEGV,handle_seg_fault);
 	do{
-		sigsetjmp(env,1);
-		char* data = (char*)current_memory_pointer;
-		char dereferenced_data = &data;
+		// printf("Current memory %d\n", current_memory_pointer);
+		sigsetjmp(env, 1);
+		char* data = (char *) current_memory_pointer;
+		int value = (int) data[0];
+		printf("%d",value);
 		current_memory_pointer+=PAGE_SIZE;
-	}while(current_memory_pointer>0)
+	}while(current_memory_pointer>0);
 	
 };
 int get_mem_diff (struct memregion *regions, unsigned int howmany,
 struct memregion *thediff, unsigned int diffsize);
+
+int main(int argc, char const *argv[])
+{
+	get_mem_layout(NULL, 12);
+	return 0;
+}
