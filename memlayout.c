48f120920b84644dfdba852951965124901123b0
Kdan <kdanhillier@hotmail.co.uk>
2016-09-26 16:43:10 -0600

    we read a thing or two!

--- memlayout.c
+++ memlayout.c
@@ -10,7 +10,7 @@
 #define MEM_NO 2
 #define PAGE_SIZE 4096
 
-unsigned int current_memory_pointer = 0;
+int32_t current_memory_pointer = 0x0;
 sigjmp_buf env;
 
 struct memregion {
@@ -18,22 +18,33 @@ struct memregion {
 	void *to;
 	unsigned char mode; /* MEM_RW, or MEM_RO, or MEM_NO */
 };
+
 void handle_seg_fault(int sig)
 {
-	printf("Encountered seg fault at %d\n",current_memory_pointer);
+	// printf("Encountered seg fault at %ld\n",(long)current_memory_pointer);
 	current_memory_pointer+=PAGE_SIZE;
 	siglongjmp(env,1);
 }
 
 int get_mem_layout (struct memregion *regions, unsigned int size)
 {
-	(void) signal(SIGSEGV,handle_seg_fault);
+	printf("%d",3);
+
+	struct sigaction onSegFault;
+	onSegFault.sa_handler = handle_seg_fault;
+	sigemptyset(&onSegFault.sa_mask);
+	onSegFault.sa_flags = 1;
+
+	sigaction(SIGSEGV, &onSegFault, NULL);
 	do{
-		// printf("Current memory %d\n", current_memory_pointer);
+		if (current_memory_pointer < 0) {break;}
 		sigsetjmp(env, 1);
-		char* data = (char *) current_memory_pointer;
-		int value = (int) data[0];
-		printf("%d",value);
+
+		// printf("Current memory %ld\n", current_memory_pointer);
+		char data = * ((char*) current_memory_pointer);
+		// printf("%c",value);
+		// printf("no fault at %d\n", current_memory_pointer);
+		printf("Read at %ld\n", (long) current_memory_pointer);
 		current_memory_pointer+=PAGE_SIZE;
 	}while(current_memory_pointer>0);
 	
@@ -43,6 +54,9 @@ struct memregion *thediff, unsigned int diffsize);
 
 int main(int argc, char const *argv[])
 {
+	printf("%d",1);
 	get_mem_layout(NULL, 12);
+	
+	printf("%d",2);
 	return 0;
 }
