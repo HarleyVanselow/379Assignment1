e4255d263948f7c8a2031baf8e28ef3aa11abf6c
Kdan <kdanhillier@hotmail.co.uk>
2016-09-27 15:49:51 -0600

    hex addresses and human readable mode

--- memlayout.c
+++ memlayout.c
@@ -96,12 +96,22 @@ int main(int argc, char const *argv[])
 {
 	struct memregion * ptr;
 	ptr = (struct memregion*)malloc(sizeof(struct memregion)*30);
-	
+
 	int number_of_regions = get_mem_layout(ptr, 30);
 	int i = 0;
+	printf("Numer of regions: %d\n", number_of_regions);
 	for (i; i < number_of_regions; ++i)
 	{
-		printf("start: %u, end: %u mode: %u\n", *(uint32_t *)ptr[i].from, *(uint32_t*)ptr[i].to, ptr[i].mode);
+		int mode = ptr[i].mode;
+		char * mode_text;
+		if (mode == 0){
+			mode_text = "RW";
+		} else if (mode == 1){
+			mode_text = "RO";
+		} else {
+			mode_text = "NO";
+		}
+		printf("0x%08X-0x%08X %s\n", *(uint32_t *)ptr[i].from, *(uint32_t*)ptr[i].to, mode_text);
 	}
 	
 	return 0;
