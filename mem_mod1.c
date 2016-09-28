cfa9481f5f215485474d749d4618f69ebc9de100
HarleyVanselow <code@harleyv.net>
2016-09-27 18:24:27 -0600

    mem_mod1 code

--- mem_mod1.c
+++ mem_mod1.c
@@ -1,5 +1,5 @@
 #include "memlayout.h"
-
+#include <sys/mman.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -10,10 +10,13 @@ int main(int argc, char const *argv[])
 	struct memregion * ptr;
 	ptr = (struct memregion*)malloc(sizeof(struct memregion)*30);
 
+	struct memregion * diffptr;
+	diffptr = (struct memregion*)malloc(sizeof(struct memregion)*30);
+
 	int number_of_regions = get_mem_layout(ptr, 30);
-	int i = 0;
-	printf("Numer of regions: %d\n", number_of_regions);
-	for (i; i < number_of_regions; ++i)
+	int i;
+	printf("Number of regions: %d\n", number_of_regions);
+	for (i=0; i < number_of_regions; ++i)
 	{
 		int mode = ptr[i].mode;
 		char * mode_text;
@@ -24,7 +27,26 @@ int main(int argc, char const *argv[])
 		} else {
 			mode_text = "NO";
 		}
-		// printf("0x%08X-0x%08X %s\n", *(uint32_t *)ptr[i].from, *(uint32_t*)ptr[i].to, mode_text);
+		printf("0x%u-0x%u %s\n", *(uint32_t *)ptr[i].from, *(uint32_t*)ptr[i].to, mode_text);
+	}
+
+	//Change mem
+	mmap(0x00000000, 4096, PROT_READ, MAP_SHARED, -1, 0);
+
+	int new_number_of_regions = get_mem_diff(ptr,number_of_regions,diffptr,30);
+
+	printf("Number of regions: %d\n", number_of_regions);
+	for (i=0; i < new_number_of_regions; ++i)
+	{
+		int mode = ptr[i].mode;
+		char * mode_text;
+		if (mode == 0){
+			mode_text = "RW";
+		} else if (mode == 1){
+			mode_text = "RO";
+		} else {
+			mode_text = "NO";
+		}
 		printf("0x%u-0x%u %s\n", *(uint32_t *)ptr[i].from, *(uint32_t*)ptr[i].to, mode_text);
 	}
 	
